4f5a9e8f3975f55d7fa32fdbd5e1213462c72697
Bhagyshri Saxena <bhagyshrisaxena97@gmail.com>
2026-03-25 18:06:55 -0400

    changed security config

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.cors.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -15,13 +17,22 @@ public class SecurityConfig {
         System.out.println("✅ Security FULLY DISABLED");
 
         http
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowCredentials(true);
+                config.setAllowedOrigins(List.of(
+                    "https://69c3c62b0c7bd80df9316141--frontend-emp-management.netlify.app"
+                ));
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                return config;
+            }))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             )
             .httpBasic(httpBasic -> httpBasic.disable())
-            .formLogin(form -> form.disable())
-            .securityMatcher("/**"); // 🔥 force match all
+            .formLogin(form -> form.disable());
 
         return http.build();
     }
